cb888e0f89be896dabd437d0d6f40b50c113dee6
Wanjun Tang <tangwanjun15@163.com>
2020-05-10 22:55:17 +0800

    Update download-files.py

--- download-files.py
+++ download-files.py
@@ -23,21 +23,24 @@ while n < number_sheets:
         print(error)    #文件夹已存在时，返回错误
     while i < num_rows:
         row = sheet_content.row_values(i)   #返回结果是列表
-        url = str(row[0])   #读取列表第1个，转换数据类型（这里这一步非必要，已是string），正则表达式只能按照某种模式匹配字符串
+        url = str(row[0])   #读取列表第1个，转换数据类型（这里这一步非必要，已是string），正则表达式匹配字符串
         matchobject = re.match(r'https://s3plus.sankuai.com/s3forkld/(.*).zip.*', url)  #构建从url字符串的起始位置匹配压缩文件名模式
         encodestring = matchobject.group(1)  #获取压缩文件名URL编码
         filename = urllib.parse.unquote(encodestring)  #URL解码成中文字符
         file = requests.get(url)  #请求连接url
-        open(folder_path + filename + '_' + str(i+1) + '.zip', 'wb').write(file.content)  #打开文件写进url内容，文件不存在时会自动生成，即压缩包文件
+        open(folder_path + filename + '_' + str(i+1) + '.zip', 'wb').write(file.content)  #打开一个文件对象写进url内容，文件不存在时会自动生成，即压缩包文件
         i = i + 1
     print(worksheets[n])
     n = n + 1
     i = 0
 
-
-
-
-
-
-
-
+'''
+reference
+下载文件：
+https://dzone.com/articles/simple-examples-of-downloading-files-using-python
+http://www.compciv.org/guides/python/fileio/open-and-write-files/
+正则表达式：https://www.runoob.com/python/python-reg-expressions.html 
+URL解码：https://www.urldecoder.io/python/
+xlrd模块读取.xls文件：https://www.blog.pythonlibrary.org/2014/04/30/reading-excel-spreadsheets-with-python-and-xlrd/
+创建文件夹：https://www.geeksforgeeks.org/create-a-directory-in-python/
+'''
